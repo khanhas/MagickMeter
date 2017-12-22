07ea353f21283b7ea219f07ea105077af54f3683
Nguyen Xuan Khanh <xuankhanh963@gmail.com>
2017-12-22 12:23:41 +0700

    Added MagickPI, Magick2PI, MagickPI2

--- src/MagickMeter.h
+++ src/MagickMeter.h
@@ -14,7 +14,9 @@
 typedef std::vector<std::wstring> WSVector;
 #define INVISIBLE Magick::Color("transparent")
 #define ONEPIXEL Magick::Image(Magick::Geometry(1,1), INVISIBLE)
-
+#define MagickPI2	1.57079632679489661923132169163975144209858469968755
+#define MagickPI	3.14159265358979323846264338327950288419716939937510
+#define Magick2PI	6.28318530717958647692528676655900576839433879875020
 struct ImgStruct
 {
 	Magick::Image	contain = ONEPIXEL;

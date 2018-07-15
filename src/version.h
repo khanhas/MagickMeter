fd1fa9ef11c1b62748f223ff42d1195420103e59
khanhas <xuankhanh963@gmail.com>
2018-07-15 16:43:40 +0700

    Update resource files

---
+++ src/version.h
@@ -0,0 +1,9 @@
+#define PLUGIN_VERSION "0.4.0.0"
+#define PLUGIN_AUTHOR "khanhas"
+#define PLUGIN_YEAR 2018
+// Has to be the same as the project name!
+#define PLUGIN_NAME "MagickMeter"
+// This is the path relative to root directory (where build.bat is placed)
+// #define EXAMPLE_SKIN_PATH "ExampleSkin"
+// #define EXAMPLE_SKIN_NAME "Example Skin"
+#define RAINMETER_VERSION "4.0.0.0"

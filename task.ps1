ecf85cb15552ec9dbccf8bfbcd1ce95f161b61db
khanhas <xuankhanh963@gmail.com>
2018-07-29 14:23:51 +0700

    Distribute scripts

--- task.ps1
+++ task.ps1
@@ -17,3 +17,32 @@ function build() {
     #Build x86
     &$msbuild /p:Platform=Win32
 }
+
+function dist() {
+    $version = "0.5.0"
+    Get-ChildItem -Recurse -File -Path ".\build" |
+        Where-Object {$_.Extension -notmatch ".dll" } |
+        Foreach-Object { Remove-Item $_.FullName }
+
+    Compress-Archive -Path ".\build\*" ".\build\magickmeter-$($version)-dll-x64-x86.zip"
+}
+
+function bumpVersion() {
+    param (
+        [Parameter(Mandatory = $true)][int16]$major,
+        [Parameter(Mandatory = $true)][int16]$minor,
+        [Parameter(Mandatory = $true)][int16]$patch
+    )
+
+    $ver = "$($major).$($minor).$($patch)"
+
+    (Get-Content ".\src\version.h") -replace "PLUGIN_VERSION `"[\d\.]*`"", "PLUGIN_VERSION `"$($ver).0`"" |
+        Set-Content ".\src\version.h"
+
+    (Get-Content ".\task.ps1") -replace "version = `"[\d\.]*`"", "version = `"$($ver)`"" |
+        Set-Content ".\task.ps1"
+
+    $ver2 = "$($major),$($minor),$($patch)"
+    (Get-Content ".\src\Magickmeter.rc") -replace "FILEVERSION [\d,]*", "FILEVERSION $($ver2),0" |
+        Set-Content ".\src\Magickmeter.rc"
+}
